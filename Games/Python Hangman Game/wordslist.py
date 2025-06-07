5e33d7a4236d5ed2ea43d2a6f98c2de1c50c364a
abinashprasana <abinashprasana400@gmail.com>
2025-06-07 20:33:54 +0530

    Add files via upload

---
+++ Games/Python Hangman Game/wordslist.py
@@ -0,0 +1,8 @@
+words = (
+    "apple", "banana", "grape", "tiger", "lion", "elephant", "peach", "cherry", "mango", "zebra",
+    "giraffe", "penguin", "lemon", "plum", "watermelon", "kiwi", "orange", "papaya", "coconut", "dragonfruit",
+    "truck", "laptop", "keyboard", "pencil", "notebook", "bottle", "blanket", "window", "mirror", "chair",
+    "river", "mountain", "desert", "forest", "ocean", "island", "village", "castle", "temple", "bridge",
+    "eagle", "parrot", "rabbit", "kangaroo", "whale", "dolphin", "shark", "octopus", "jellyfish", "crab",
+    "panda", "koala", "buffalo", "camel", "peacock", "raccoon", "hamster", "donkey", "squirrel", "moose"
+)
